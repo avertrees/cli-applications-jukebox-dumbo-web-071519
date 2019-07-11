a9084ea82451abdeee97e2d5dbce0e396c797899
Alessandra Vertrees <avertrees447@gmail.com>
2019-07-11 15:42:50 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,61 @@
 # Add your code here
+def help
+  puts "I accept the following commands:"
+  puts " - help : displays this help message"
+  puts " - list : displays a list of songs you can play"
+  puts " - play : lets you choose a song to play"
+  puts " - exit : exits the program"
+
+end
+
+class String
+  def is_integer?
+    self.to_i.to_s == self
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number"
+  usrRequest = gets.chomp
+  puts usrRequest
+  if songs.include?(usrRequest)
+      puts "Playing #{usrRequest}"
+  elsif usrRequest.is_integer? && usrRequest.to_i < songs.length
+    #puts usrRequest
+    index = usrRequest.to_i-1
+    puts "Playing #{songs.at(index)}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index + 1}. #{song}"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs) 
+  help
+
+  puts "Please enter a command:"
+  input = gets.chomp
+  if input != "exit"
+    #puts input
+    case input
+    when "help"
+      help
+    when "list"
+      list(songs)
+    when "play"
+      play(songs)
+    else 
+      puts "invalid"
+    end
+  end 
+  exit_jukebox
+end
